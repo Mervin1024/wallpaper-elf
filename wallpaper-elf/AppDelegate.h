40d2df03ad404c177e1deb4d526adebe239705fa
马遥 <mervin1024@163.com>
2016-07-11 12:01:41 +0800

    Initial Commit

---
+++ wallpaper-elf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wallpaper-elf
+//
+//  Created by mervin on 16/7/11.
+//  Copyright © 2016年 mervin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wallpaper-elf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wallpaper-elf
+//
+//  Created by mervin on 16/7/11.
+//  Copyright © 2016年 mervin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
